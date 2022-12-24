80ee7675a7ea097feb053910cec8aabf42370e76
vanyarudchenko89 <112881427+vanyarudchenko89@users.noreply.github.com>
2022-12-24 14:59:45 +0000

    Add files via upload

---
+++ _init_.py
@@ -0,0 +1,20 @@
+def create_error():
+    z = input("Error Text ")
+    print(z)
+
+#by dafre
+
+def load_tk():
+    import tkinter
+    tk = tkinter
+    print("import completed")
+def create_dialog():
+    d = input("Dialog Text ")
+    print(d)
+
+def about():
+    print("Python+ Version 1.0 build 10596")
+
+def load_time():
+    import time
+    print("import completed")
